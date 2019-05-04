f04ffde132e6e85d7c11086063559b0e562f1698
omobukki <42176174+omobukki@users.noreply.github.com>
2019-05-04 11:41:45 -0500

    Updates

---
+++ precache-manifest.393f06274083dc015139cc819d6833f2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fab82da4d9420c34bc57",
+    "url": "/Clicky-Game/static/js/runtime~main.012bace8.js"
+  },
+  {
+    "revision": "8d4f2a149ca8cf1dc614",
+    "url": "/Clicky-Game/static/js/main.8c90616f.chunk.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/Clicky-Game/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "8d4f2a149ca8cf1dc614",
+    "url": "/Clicky-Game/static/css/main.9ae94903.chunk.css"
+  },
+  {
+    "revision": "50994754d36fb56f7628a0a569bd4514",
+    "url": "/Clicky-Game/index.html"
+  }
+];
